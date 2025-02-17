2aabd617469db352ae089a9e70b3e7e32e77ae80
Jamie Kozminska <jamie.kozminska@docscentre.com.au>
2025-02-17 21:28:08 +1100

    Placeholder image if there are no images from api.

--- src/components/FeedbackForm.vue
+++ src/components/FeedbackForm.vue
@@ -142,7 +142,6 @@ const submitFeedback = async () => {
 <style scoped>
 .feedback-modal {
   width: 100%;
-  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -11,13 +11,21 @@
           :space-between="0"
           @swiper="setSwiper"
         >
-          <vue-swiper-slide v-for="(photo, index) in restaurantData.photos" :key="index">
-            <img :src="getPhotoUrl(photo.photo_reference)" :alt="`${restaurantData.name} photo ${index + 1}`"
-              class="meal-image" @error="handleImageError" />
-          </vue-swiper-slide>
+          <template v-if="restaurantData.photos && restaurantData.photos.length > 0">
+            <vue-swiper-slide v-for="(photo, index) in restaurantData.photos" :key="index">
+              <img :src="getPhotoUrl(photo.photo_reference)" :alt="`${restaurantData.name} photo ${index + 1}`"
+                class="meal-image" @error="handleImageError" />
+            </vue-swiper-slide>
+          </template>
+          <template v-else>
+            <vue-swiper-slide>
+              <img :src="placeholderImage" :alt="restaurantData.name" class="meal-image" />
+            </vue-swiper-slide>
+          </template>
         </vue-swiper>
-        <!-- Add navigation button overlays -->
-        <div class="navigation-buttons">
+        
+        <!-- Only show navigation if we have multiple photos -->
+        <div class="navigation-buttons" v-if="restaurantData.photos && restaurantData.photos.length > 1">
           <button class="nav-button prev" @click.stop="navigatePrev">
             <ion-icon :icon="chevronBack"></ion-icon>
           </button>
@@ -60,13 +68,14 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { IonSpinner, IonCard, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/vue';
+import { IonSpinner, IonCard, IonCardTitle, IonCardSubtitle, IonCardContent, IonIcon } from '@ionic/vue';
 import type { Restaurant } from '@/types/restaurant';
 import { Swiper as VueSwiper, SwiperSlide as VueSwiperSlide } from 'swiper/vue';
 import { chevronBack, chevronForward } from 'ionicons/icons';
 import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import placeholderImage from '@/assets/meal-placeholder.png';
 
 const swiper = ref(null);
 const swiperModules = [Pagination];
@@ -89,19 +98,14 @@ function setSwiper(swiperInstance: any) {
 
 function getPhotoUrl(photoReference?: string) {
   if (!photoReference) {
-    return `<svg fill="#000000" version="1.1" width="800px" height="800px" viewBox="0 0 302.459 302.46" xml:space="preserve">
-<g>
-	<g>
-		<path d="M163.816,222.91c38.745,0,70.269-31.514,70.269-70.259c0-38.745-31.523-70.274-70.269-70.274    c-38.743,0-70.264,31.524-70.264,70.274C93.552,191.396,125.068,222.91,163.816,222.91z"/>
-		<path d="M163.816,241.771c49.226,0,89.118-39.903,89.118-89.119c0-49.228-39.893-89.123-89.118-89.123    c-49.221,0-89.119,39.895-89.119,89.123C74.697,201.867,114.595,241.771,163.816,241.771z M163.816,77.183    c41.608,0,75.469,33.862,75.469,75.469c0,41.609-33.86,75.464-75.469,75.464c-41.609,0-75.464-33.854-75.464-75.464    C88.352,111.045,122.207,77.183,163.816,77.183z"/>
-		<path d="M302.459,250.062l-5.2-200.026c0-4.307-3.493-7.8-7.8-7.8c-1.152,0-2.234,0.264-3.214,0.718v-0.074    c0,0-0.203,0.13-0.493,0.328c-0.746,0.412-1.416,0.919-1.995,1.539c-5.708,4.685-23.907,23.78-25.431,49.592    c-2.311,39.372,16.813,55.317,23.328,69.921l-5.199,85.798c0,4.306,8.693,7.8,12.999,7.8    C293.766,257.862,302.459,254.369,302.459,250.062z"/>
-		<path d="M13.614,128.206l13.461,6.724c1.534,0.769,3.225,1.33,4.992,1.747l-5.2,117.007c0,3.615,8.696,6.54,13,6.54    c4.306,0,13-2.925,13-6.54l-5.2-117.606c1.831-0.576,3.595-1.279,5.139-2.224l9.973-6.073c7.196-4.39,12.619-14.045,12.619-22.475    V52.88c0-4.307-3.494-7.8-7.8-7.8c-4.304,0-7.8,3.493-7.8,7.8v47.225h-4.332V52.88c0-4.307-3.494-7.8-7.8-7.8    c-4.304,0-7.8,3.493-7.8,7.8v47.225h-4.347V52.88c0-4.307-3.494-7.8-7.8-7.8c-4.304,0-7.8,3.493-7.8,7.8v47.225H15.6V54.565    c0-4.306-3.494-7.8-7.8-7.8c-4.304,0-7.8,3.494-7.8,7.8v51.612C0,114.852,5.855,124.327,13.614,128.206z"/>
-	</g>
-</g>
-</svg>`;
+    return placeholderImage;
   }
   return `https://maps.googleapis.com/maps/api/place/photo?height=500&maxwidth=800&photo_reference=${photoReference}&key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&t=${Date.now()}`;
+}
 
+function handleImageError(event: Event) {
+  const img = event.target as HTMLImageElement;
+  img.src = placeholderImage;
 }
 
 const navigatePrev = () => {
@@ -115,19 +119,6 @@ const navigateNext = () => {
     swiper.value.slideNext();
   }
 };
-function handleImageError(event: Event) {
-  const img = event.target as HTMLImageElement;
-  img.src = `<svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="800px" height="800px" viewBox="0 0 302.459 302.46" xml:space="preserve">
-<g>
-	<g>
-		<path d="M163.816,222.91c38.745,0,70.269-31.514,70.269-70.259c0-38.745-31.523-70.274-70.269-70.274    c-38.743,0-70.264,31.524-70.264,70.274C93.552,191.396,125.068,222.91,163.816,222.91z"/>
-		<path d="M163.816,241.771c49.226,0,89.118-39.903,89.118-89.119c0-49.228-39.893-89.123-89.118-89.123    c-49.221,0-89.119,39.895-89.119,89.123C74.697,201.867,114.595,241.771,163.816,241.771z M163.816,77.183    c41.608,0,75.469,33.862,75.469,75.469c0,41.609-33.86,75.464-75.469,75.464c-41.609,0-75.464-33.854-75.464-75.464    C88.352,111.045,122.207,77.183,163.816,77.183z"/>
-		<path d="M302.459,250.062l-5.2-200.026c0-4.307-3.493-7.8-7.8-7.8c-1.152,0-2.234,0.264-3.214,0.718v-0.074    c0,0-0.203,0.13-0.493,0.328c-0.746,0.412-1.416,0.919-1.995,1.539c-5.708,4.685-23.907,23.78-25.431,49.592    c-2.311,39.372,16.813,55.317,23.328,69.921l-5.199,85.798c0,4.306,8.693,7.8,12.999,7.8    C293.766,257.862,302.459,254.369,302.459,250.062z"/>
-		<path d="M13.614,128.206l13.461,6.724c1.534,0.769,3.225,1.33,4.992,1.747l-5.2,117.007c0,3.615,8.696,6.54,13,6.54    c4.306,0,13-2.925,13-6.54l-5.2-117.606c1.831-0.576,3.595-1.279,5.139-2.224l9.973-6.073c7.196-4.39,12.619-14.045,12.619-22.475    V52.88c0-4.307-3.494-7.8-7.8-7.8c-4.304,0-7.8,3.493-7.8,7.8v47.225h-4.332V52.88c0-4.307-3.494-7.8-7.8-7.8    c-4.304,0-7.8,3.493-7.8,7.8v47.225h-4.347V52.88c0-4.307-3.494-7.8-7.8-7.8c-4.304,0-7.8,3.493-7.8,7.8v47.225H15.6V54.565    c0-4.306-3.494-7.8-7.8-7.8c-4.304,0-7.8,3.494-7.8,7.8v51.612C0,114.852,5.855,124.327,13.614,128.206z"/>
-	</g>
-</g>
-</svg>`;
-}
 
 </script>
 
